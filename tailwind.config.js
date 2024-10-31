423cc94765e7c9ceba50443b1196ed23e47a31e9
Aniket Kumar <kumaraniket9353@gmail.com>
2024-10-31 22:32:33 +0530

    Added demo folder

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind will look for classes in these files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
